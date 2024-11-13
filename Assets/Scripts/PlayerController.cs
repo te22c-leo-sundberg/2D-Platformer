58fad9179083f5a50933a6beeb87786b3a003d73
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-11-13 16:20:03 +0100

    Addade ett objekt som vid kollision boostar din dash. Gjorde en sprite för en dashande slime och vanlig slime.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,21 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed = 7;
-    float movespeed = 5;
+    // float movespeed = 5;
     [SerializeField]
     float jumpForce = 10;
     bool canJump = true;
-
+    [SerializeField] public float dashBoostTime;
     bool isFacingRight = true;
+    int dashDirection = 0;
     private float xMove;
-
     bool canDash = true;
     bool isDashing;
+    public bool IsDashBoosted = false;
     [SerializeField] float dashPower = 24f;
     [SerializeField] float dashTime = 0.2f;
+
+    float originalDashTime;
     [SerializeField] float dashCooldown = 1f;
 
     [SerializeField]
@@ -27,7 +30,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     LayerMask groundLayer;
     [SerializeField] Rigidbody2D rb;
-
+    [SerializeField] Sprite SlimeSprite;
+    [SerializeField] Sprite DashSlimeSprite;
+    public void Start()
+    {
+        originalDashTime = dashTime;
+    }
     void Update()
     {
         if (isDashing)
@@ -46,10 +54,12 @@ public class PlayerController : MonoBehaviour
         if (rb.velocity.x < 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
+            isFacingRight = false;
         }
         if (rb.velocity.x > 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
+            isFacingRight = true;
         }
 
         if (Input.GetAxisRaw("Jump") > 0 && canJump && IsGrounded())
@@ -66,18 +76,15 @@ public class PlayerController : MonoBehaviour
         {
             StartCoroutine(Dash());
         }
-        Flip();
-    }
-    private void Flip()
-    {
-        if (isFacingRight && xMove < 0f || !isFacingRight && xMove > 0f)
+        if (IsDashBoosted)
         {
-            Vector3 localScale = transform.localScale;
-            isFacingRight = !isFacingRight;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+            Timers();
+            DashBooster();
         }
+        else
+        {}
     }
+
     private bool IsGrounded()
     {
         if (Physics2D.OverlapCircle(groundChecker.position, .2f, groundLayer))
@@ -91,15 +98,44 @@ public class PlayerController : MonoBehaviour
     }
     private IEnumerator Dash() //make rythm game dash? every x seconds your character blinks, dash close to that timeframe and get a stronger dash
     {
+        if (isFacingRight)
+        {
+            dashDirection = 1;
+        }
+        else if (!isFacingRight)
+        {
+            dashDirection = -1;
+        }
         canDash = false;
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0;
-        rb.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        rb.velocity = new Vector2(dashDirection * dashPower, 0f);
+        GetComponent<SpriteRenderer>().sprite = DashSlimeSprite;
         yield return new WaitForSeconds(dashTime);
         rb.gravityScale = originalGravity;
+        GetComponent<SpriteRenderer>().sprite = SlimeSprite;
         isDashing = false;
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
+    }
+    public void Timers()
+    {
+        dashBoostTime -= Time.deltaTime;
+    }
+    public void DashBooster()
+    {
+        if (dashBoostTime > 0)
+        {
+            dashTime = 0.2f;
+            print("I dash further!");
+        }
+        else if (dashBoostTime <= 0)
+        {
+            dashTime = originalDashTime;
+            print("I stopped dashing further!");
+            IsDashBoosted = false;
+            dashBoostTime = 0.5f;
+        }
     }
 }
